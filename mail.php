697b39787b81ad121cd306432cdecbf26e896784
Gabriel Garrett <gabegarrett@Gabes-MacBook-Air.local>
2014-02-06 12:09:12 -0500

    3D Printer FAQ Website
    A website I made for my HTML/CSS web development class.

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+//$phone = $_POST['phone'];
+//$call = $_POST['call'];
+$website = $_POST['website'];
+$priority = $_POST['priority'];
+$type = $_POST['type'];
+$message = $_POST['message'];
+$formcontent=" From: $name \n Website: $website \n Priority: $priority \n Type: $type \n Message: $message";
+$recipient = "garrga@farmingdale.edu";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!" . " -" . "";
+?>
+
+<html>
+<body>
+<a href="index.html"><div><li>Return Home</li></div></a>
+</body>
+</html>
